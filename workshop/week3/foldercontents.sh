e874032cc55fa89b7021c933c3efd9b3a4c8886d
ANDREW <Andrew-McM@github.com>
2020-03-21 05:45:43 +0000

    Initial project added

--- workshop/week3/foldercontents.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#This line will prompt the user for input.
-read -p "type the name of the folder you wish to display the contents of:" folderName
-
-#This line will display the folder contents of the folder input by user.
-ls ~/CSP2101/workshop/cd"$folderName"./
